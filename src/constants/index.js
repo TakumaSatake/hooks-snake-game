b760b9413f18045d986cd86ab02e217616bb45b0
TakumaSatake <98210198+TakumaSatake@users.noreply.github.com>
2022-01-30 18:14:50 +0900

    SnakeGame Refuctring

---
+++ src/constants/index.js
@@ -0,0 +1,44 @@
+import { initFields } from "../utils";
+
+const fieldSize = 35;
+export const initalPosition = { x: 17, y: 17 };
+export const initialValues = initFields(fieldSize, initalPosition);
+export const defaultInterval = 100;
+export const defaultDifficulty = 3;
+
+export const Difficulty = [1000, 500, 100, 50, 10];
+
+export const GameStatus = Object.freeze({
+  init: "init",
+  playing: "playing",
+  suspended: "suspended",
+  gemeover: "gameover"
+});
+
+export const Direction = Object.freeze({
+  up: "up",
+  right: "right",
+  left: "left",
+  down: "down"
+});
+
+export const Delta = Object.freeze({
+  up: { x: 0, y: -1 },
+  right: { x: 1, y: 0 },
+  left: { x: -1, y: 0 },
+  down: { x: 0, y: 1 }
+});
+
+export const DirectionKeyCodeMap = Object.freeze({
+  37: Direction.left,
+  38: Direction.up,
+  39: Direction.right,
+  40: Direction.down
+});
+
+export const OppositeDirection = Object.freeze({
+  up: "down",
+  right: "left",
+  left: "right",
+  down: "up"
+});
